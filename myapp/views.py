32e7a5d72faba13d1e14944cdf6545f1fb4a6d5e
Shakti Nath Saini <shaktinathsaini.sns@gmail.com>
2017-07-24 11:37:37 +0530

    Changes made

--- myapp/views.py
+++ myapp/views.py
@@ -1,26 +1,70 @@
 from django.shortcuts import render, redirect
-from forms import SignUpForm
-from models import UserModel
-from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from forms import SignUpForm, LoginForm
+from models import UserModel, SessionToken
+from django.contrib.auth.hashers import make_password, check_password
+from datetime import timedelta
+from django.utils import timezone
+
 
 # Create your views here.
 
 
 def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            #saving data to DB
+            user = UserModel(name=name, password=make_password(password), email=email, username=username)
+            user.save()
+            return render(request, 'success.html')
+            #return redirect('login/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'index.html', {'form' : form})
+
+def login_view(request):
+
+  response_data = {}
   if request.method == "POST":
-    form = SignUpForm(request.POST)
+    form = LoginForm(request.POST)
     if form.is_valid():
-      username = form.cleaned_data['username']
-      name = form.cleaned_data['name']
-      email = form.cleaned_data['email']
-      password = form.cleaned_data['password']
-      #saving data to DB
-      user = UserModel(name=name, password=make_password(password), email=email, username=username)
-      user.save()
-      return render(request, 'success.html')
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = UserModel.objects.filter(username=username).first()
 
-  elif request.method == "GET":
-    form = SignUpForm()
+      if user:
+        if check_password(password, user.password):
+          token = SessionToken(user=user)
+          token.create_token()
+          token.save()
+          response = redirect('feeds/')
+          response.set_cookie(key='session_token', value=token.session_token)
+          return response
+        else:
+          response_data['message'] = 'Incorrect Password! Please try again!'
 
-  return render(request, 'index.html', {'form' : form})
+  elif request.method == 'GET':
+    form = LoginForm()
+
+  response_data['form'] = form
+  return render(request, 'login.html', response_data)
+
+
+def feed_view(request):
+
+  return render(request, 'feeds.html')
+
+
+# For validating the session
+def check_validation(request):
+  if request.COOKIES.get('session_token'):
+    session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+    if session:
+      return session.user
+  else:
+    return None
